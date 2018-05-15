a4a0e1c75fb521e1645ba23d11f9f385714e3704
agpiermarini <piermarini.andrew@gmail.com>
2018-05-15 10:33:59 -0600

    Remove redundant ActionMailer settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
 
+  # Configure MailCatcher
+  config.action_mailer.smtp_settings = { :address => '127.0.0.1', :port => 1025 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -51,8 +54,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Configure MailCatcher
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 end
