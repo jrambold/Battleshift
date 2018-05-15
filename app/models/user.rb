ed2b5c27c5e7f39c780fa1b6694f0b32878b8c8d
jrambold <jrambold@gmail.com>
2018-05-15 12:15:18 -0600

    activation works and email contains necessary info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,21 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, :username, presence: true, uniqueness: true
-  validates :password, presence: true
-  before_save :set_keys
+  validates :password, presence: true, on: :create
 
   def activate(url_token)
     if url_token == self.token
-      self.active = true
+      self.update!(active: true)
     end
   end
 
-  private
-    def set_keys
-      self.api_key = generate_key
-      self.token = generate_key
-    end
+  def set_keys
+    self.api_key = generate_key
+    self.token = generate_key
+  end
 
+  private
     def generate_key
       SecureRandom.urlsafe_base64
     end
---
+++ spec/features/user_can_activate_account_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'User' do
+  describe 'fills out a registration form and goes to activation link' do
+    it 'and it activates an account' do
+      visit '/register'
+
+      fill_in 'user[email]', with: 'email@example.com'
+      fill_in 'user[username]', with: 'username'
+      fill_in 'user[password]', with: 'password'
+      fill_in 'user[password_confirmation]', with: 'password'
+      click_on 'Create User'
+
+      user = User.last
+
+      expect(user.active).to be_falsey
+
+      visit "/activate?id=#{user.id}&token=#{user.token}"
+
+      expect(User.last.active).to be_truthy
+
+    end
+  end
+end
