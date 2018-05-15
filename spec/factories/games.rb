e417061da8f8224d229a7f575e41ee231b9afa58
agpiermarini <piermarini.andrew@gmail.com>
2018-05-15 16:23:55 -0600

    Update game factory to have user association. Update user_can_see_a_game_spec to pass in player id as part of game attributes

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -5,6 +5,8 @@ FactoryBot.define do
     winner nil
     player_1_turns 0
     player_2_turns 0
+    association :player_1, factory: :user
+    association :player_2, factory: :user
     current_turn "player_1"
   end
 end
