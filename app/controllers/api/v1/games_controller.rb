260cd5338eb3ee5c86f8553ea4a46a649c567554
agpiermarini <piermarini.andrew@gmail.com>
2018-05-15 16:14:30 -0600

    Add GamesController#create. Add player_1 and player_2 user foreign keys to game. Add appropriate associations to user and game. Add tests for associations.

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,6 +1,12 @@
 module Api
   module V1
     class GamesController < ApiController
+      def create
+        game = Game.start(params[:opponent_email], request.headers.env["HTTP_X_API_KEY"])
+  
+        render json: game
+      end
+
       def show
         game = Game.find(params[:id])
         render json: game
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of :password }
     it { should validate_uniqueness_of :username }
     it { should validate_uniqueness_of :email }
+    it { should have_many :player_1s }
+    it { should have_many :player_2s }
   end
 
   describe 'class methods' do
