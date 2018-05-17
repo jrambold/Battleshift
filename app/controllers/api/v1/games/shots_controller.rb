75dc6c2d1726fdca28cd116ae24054930713f7e2
jrambold <jrambold@gmail.com>
2018-05-17 07:23:10 -0600

    ships handle proper player and shots can be made by both players alternating

--- app/controllers/api/v1/games/shots_controller.rb
+++ app/controllers/api/v1/games/shots_controller.rb
@@ -4,11 +4,23 @@ module Api
       class ShotsController < ApiController
         def create
           game = Game.find(params[:game_id])
+          if game.player_1.api_key == request.headers["HTTP_X_API_KEY"]  && game.current_turn == "player_1"
 
-          turn_processor = TurnProcessor.new(game, params[:shot][:target])
+            turn_processor = TurnProcessor.new(game, params[:shot][:target])
 
-          turn_processor.run!
-          render json: game, message: turn_processor.message
+            turn_processor.run!
+            render json: game, message: turn_processor.message
+          elsif game.player_2.api_key == request.headers["HTTP_X_API_KEY"] && game.current_turn == "player_2"
+             game.player_1.api_key == request.headers["HTTP_X_API_KEY"]
+
+            turn_processor = TurnProcessor.new(game, params[:shot][:target])
+
+            turn_processor.run!
+            render json: game, message: turn_processor.message
+          else
+            game.messages = "Invalid move. It's your opponent's turn"
+            render json: game, message: "#{game.messages}"
+          end
         end
       end
     end
--- app/services/turn_processor.rb
+++ app/services/turn_processor.rb
@@ -7,8 +7,11 @@ class TurnProcessor
 
   def run!
     begin
-      attack_opponent
-      ai_attack_back
+      if game.current_turn == "player_1"
+        attack_player_2
+      else
+        attack_player_1
+      end
       game.save!
     rescue InvalidAttack => e
       @messages << e.message
@@ -23,23 +26,25 @@ class TurnProcessor
 
   attr_reader :game, :target
 
-  def attack_opponent
-    result = Shooter.fire!(board: opponent.board, target: target)
+  def attack_player_2
+    result = Shooter.fire!(board: player_2.board, target: target)
     @messages << "Your shot resulted in a #{result}."
     game.player_1_turns += 1
+    game.current_turn = "player_2"
   end
 
-  def ai_attack_back
-    result = AiSpaceSelector.new(player.board).fire!
-    @messages << "The computer's shot resulted in a #{result}."
+  def attack_player_1
+    result = Shooter.fire!(board: player_1.board, target: target)
+    @messages << "Your shot resulted in a #{result}."
     game.player_2_turns += 1
+    game.current_turn = "player_1"
   end
 
-  def player
+  def player_1
     Player.new(game.player_1_board)
   end
 
-  def opponent
+  def player_2
     Player.new(game.player_2_board)
   end
 
