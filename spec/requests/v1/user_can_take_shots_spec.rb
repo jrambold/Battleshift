c3b8a14e43b618cd7a96e378868488bf011741a2
jrambold <jrambold@gmail.com>
2018-05-20 21:17:16 -0600

    cannot fire until ships are placed

--- spec/requests/v1/user_can_take_shots_spec.rb
+++ spec/requests/v1/user_can_take_shots_spec.rb
@@ -9,7 +9,7 @@ describe "Api::V1::Shots" do
     let(:sm_ship) { Ship.new(2) }
 
     it "updates the message and board with a miss for player_1" do
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_1_header
 
@@ -26,7 +26,7 @@ describe "Api::V1::Shots" do
     end
 
     it "updates the message and board with a miss for both player_2" do
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2")
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2", player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_2_header
 
@@ -45,7 +45,7 @@ describe "Api::V1::Shots" do
     it "updates the message but not the board with invalid coordinates for player_1" do
       player_1_board = Board.new(1)
       player_2_board = Board.new(1)
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, player_1_turns: 1)
 
       json_payload = {target: "B1"}.to_json
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_1_header
@@ -59,7 +59,7 @@ describe "Api::V1::Shots" do
     it "updates the message but not the board with invalid coordinates for player_2" do
       player_1_board = Board.new(1)
       player_2_board = Board.new(1)
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2")
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2", player_1_turns: 1)
 
       json_payload = {target: "B1"}.to_json
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_2_header
@@ -75,7 +75,7 @@ describe "Api::V1::Shots" do
                      ship: sm_ship,
                      start_space: "A1",
                      end_space: "A2").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_1_header
 
@@ -96,7 +96,7 @@ describe "Api::V1::Shots" do
                     ship: sm_ship,
                     start_space: "A1",
                     end_space: "A2").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2")
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2", player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
 
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_2_header
@@ -123,7 +123,7 @@ describe "Api::V1::Shots" do
                      ship: ship,
                      start_space: "A2",
                      end_space: "A2").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
 
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_1_header
@@ -147,7 +147,7 @@ describe "Api::V1::Shots" do
                      ship: ship,
                      start_space: "A2",
                      end_space: "A2").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2")
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2", player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
 
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_2_header
@@ -167,7 +167,7 @@ describe "Api::V1::Shots" do
                      ship: ship,
                      start_space: "A1",
                      end_space: "A1").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
 
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_1_header
@@ -187,7 +187,7 @@ describe "Api::V1::Shots" do
                      ship: ship,
                      start_space: "A1",
                      end_space: "A1").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2")
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2", player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
 
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_2_header
@@ -207,7 +207,7 @@ describe "Api::V1::Shots" do
                      ship: ship,
                      start_space: "A1",
                      end_space: "A1").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
 
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_1_header
@@ -226,7 +226,7 @@ describe "Api::V1::Shots" do
                      ship: ship,
                      start_space: "A1",
                      end_space: "A1").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2")
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2", player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
 
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_2_header
@@ -245,7 +245,7 @@ describe "Api::V1::Shots" do
         ship: ship,
         start_space: "A1",
         end_space: "A1").run
-        game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+        game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, player_1_turns: 1)
         json_payload = {target: "A1"}.to_json
 
         post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_2_header
@@ -255,7 +255,7 @@ describe "Api::V1::Shots" do
         expect(response.status).to eq(400)
         expected_messages = "Invalid move. It's your opponent's turn"
         expect(game_info[:message]).to eq expected_messages
-      end
+    end
 
     it "updates the message when player_1 tries to shoot when it's player_2s turn" do
       ship = Ship.new(1)
@@ -263,7 +263,7 @@ describe "Api::V1::Shots" do
                      ship: ship,
                      start_space: "A1",
                      end_space: "A1").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2")
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2", player_1_turns: 1)
       json_payload = {target: "A1"}.to_json
 
       post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_1_header
@@ -281,7 +281,7 @@ describe "Api::V1::Shots" do
                      ship: ship,
                      start_space: "A1",
                      end_space: "A1").run
-      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2")
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board, current_turn: "player_2", player_1_turns: 1)
       unauthorized_user = create(:user)
       unauthorized_user_header = { "CONTENT_TYPE" => "application/json", "HTTP_X_API_KEY" => unauthorized_user.api_key }
       json_payload = {target: "A1"}.to_json
@@ -293,6 +293,17 @@ describe "Api::V1::Shots" do
       expect(response.status).to eq(401)
       expected_messages = "Unauthorized"
       expect(game_info[:message]).to eq expected_messages
+    end
+
+    it "cannot fire until ships are placed" do
+      game = create(:game, player_1_board: player_1_board, player_2_board: player_2_board)
+      json_payload = {target: "A1"}.to_json
+      post "/api/v1/games/#{game.id}/shots", params: json_payload, headers: player_1_header
+
+      game_info = JSON.parse(response.body, symbolize_names: true)
+
+      expect(response.status).to eq(400)
+      expected_messages = "Ships not placed"
     end
   end
 end
